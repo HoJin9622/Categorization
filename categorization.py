320009b58ed0c2bcc5e151b0d377d53dffbd2146
HoJin9622 <kiss0104040@gmail.com>
2020-05-25 16:58:34 +0900

    유사도 측정

--- categorization.py
+++ categorization.py
@@ -1,7 +1,45 @@
-from konlpy.tag import Kkma
-from konlpy.utils import pprint
+# 품사 태깅 라이브러리
+from konlpy.tag import Okt
 
-# initialization Kkma
-kkma = Kkma()
+# cos similarity 계산을 위한 라이브러리
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
 
-pprint(kkma.nouns(u'동의대학교에서 알려드립니다. 이번학기는 코로나로인해 휴강을 합니다.'))
+# initialization Okt
+okt = Okt()
+
+# 빈도 추출 함수
+def frequency_extractor(doc):
+    set_array = set()
+    for i in doc:
+        for j in i:
+            set_array.add(j)
+    set_array = list(set_array)
+
+    bow = [[0 for i in range(len(set_array))] for j in range(len(doc))]
+
+    for i in range(len(set_array)):
+        for j in range(len(doc)):
+            bow[j][i] = doc[j].count(set_array[i])
+
+    return bow
+
+# cosine similarity 함수
+def cos_similarity(A, B):
+    return dot(A, B)/(norm(A)*norm(B))
+
+# 형용사 분석 및 품사 태깅
+doc = [
+    okt.nouns(u'저는 사과 좋아요.'),
+    okt.nouns(u'저는 바나나 좋아요.'),
+    okt.nouns(u'저는 바나나 좋아요 저는 바나나 좋아요 저는 사과 좋아요 저는 사과 좋아요 저는 키위 좋아요.'),
+    okt.nouns(u'저는 바나나 좋아요.')
+]
+
+bow = frequency_extractor(doc)
+
+print("doc1 and doc2 유사도 : ", cos_similarity(bow[0], bow[1]))
+print("doc1 and doc3 유사도 : ", cos_similarity(bow[0], bow[2]))
+print("doc2 and doc3 유사도 : ", cos_similarity(bow[1], bow[2]))
+print("doc2 and doc4 유사도 : ", cos_similarity(bow[1], bow[3]))
